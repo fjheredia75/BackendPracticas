425a257559dee838d745b3aafb4fb58d301d26d6
fjheredia75 <116654492+fjheredia75@users.noreply.github.com>
2023-03-10 13:19:07 -0600

    Commit message

--- src/main/kotlin/com/example/backendpracticas/model/Activity.kt
+++ src/main/kotlin/com/example/backendpracticas/model/Activity.kt
@@ -2,12 +2,15 @@ package com.example.backendpracticas.model
 
 import java.util.Date
 import javax.persistence.Column
+import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
+import javax.persistence.Table
 import javax.validation.constraints.NotBlank
 
-
+@Entity
+@Table(name="activity")
 class Activity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
